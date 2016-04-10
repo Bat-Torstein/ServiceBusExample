17990efaa1856f4ae0f217a942de0f4c80d70ce1
Torstein Bjørgan <tob@itema.no>
2016-04-10 13:07:39 +0200

    Initial

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.ServiceBus.Messaging;
+
+namespace BussHelvete
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // TODO
+        }
+
+        private static void Send()
+        {
+            
+        }
+
+        private static void Receive()
+        {
+
+        }
+    }
+}
